29aaf6d24ec7d45cddd9bcbb0e2134c418b51da6
masdika ID <masdikaid@gmail.com>
2022-02-27 23:19:42 +0700

    call data type function in main package

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello Worlds!")
+    numberDataType()
+    charDataType()
+    boolDataType()
+    stringDataType()
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
